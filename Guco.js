94796b7a4efee2ddafdddcaf79180062d39b10a9
Zach Gibson <zach.gibson@taillight.com>
2017-07-06 08:42:18 -0500

    Initial commit

---
+++ Guco.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import {
+  View,
+  ScrollView,
+  Image,
+  Animated,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+
+const {
+  width: deviceWidth,
+  height: deviceHeight,
+} = Dimensions.get('window');
+
+export default class Guco extends Component {
+
+  state = { animatedValue: new Animated.Value(0) }
+
+  render() {
+    const { ui, backgroundImages, backgroundColor, dividerWidth, parallaxStrength } = this.props;
+
+    return (
+        <Animated.ScrollView
+          style={{ width: (deviceWidth + dividerWidth), backgroundColor }}
+          horizontal
+          pagingEnabled
+          scrollEventThrottle={1}
+          onScroll={Animated.event(
+            [{ nativeEvent: { contentOffset: { x: this.state.animatedValue } } }],
+            { useNativeDriver: true }
+          )}
+        >
+          {
+            backgroundImages.map((uri, i) => {
+              return (
+                <View
+                  key={i}
+                  style={[styles.slideOuterContainer, { width: (deviceWidth + dividerWidth) }]}
+                >
+                  <View style={styles.slideInnerContainer}>
+                    <Animated.Image
+                      style={[styles.backgroundImage, {
+                        left: i * -parallaxStrength,
+                        transform: [
+                          {
+                            translateX: this.state.animatedValue.interpolate({
+                              inputRange: [0, (deviceWidth + dividerWidth)],
+                              outputRange: [0, parallaxStrength],
+                            })
+                          }
+                        ]
+                      }]}
+                      source={{ uri }}
+                    />
+                    <View style={{ width: 375, height: 667 }}>
+                      {ui[i]}
+                    </View>
+                  </View>
+                  <View style={{ width: 8, height: 667 }} />
+                </View>
+              );
+            })
+          }
+      </Animated.ScrollView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  slideOuterContainer: {
+    flexDirection: 'row',
+  },
+  slideInnerContainer: {
+    overflow: 'hidden',
+  },
+  backgroundImage: {
+    position: 'absolute',
+    width: deviceWidth,
+    height: deviceHeight,
+  },
+});
---
+++ index.ios.js
@@ -0,0 +1,74 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+import Guco from './Guco';
+
+const views = [
+    { title: 'Guco' },
+    { title: 'McGucci' },
+    { title: 'Go Hord' }
+];
+
+const images = [
+  'https://images.unsplash.com/photo-1493246507139-91e8fad9978e?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=',
+  'https://images.unsplash.com/photo-1459909633680-206dc5c67abb?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=&bg=',
+  'https://images.unsplash.com/photo-1490237014491-822aee911b99?dpr=2&auto=format&fit=crop&w=1500&h=1001&q=80&cs=tinysrgb&crop=&bg='
+];
+
+export default class ParallaxSwiper extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Guco
+          dividerWidth={4}
+          parallaxStrength={300}
+          backgroundColor="white"
+          backgroundImages={images}
+          ui={
+            views.map((view, i) => (
+              <View key={i} style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Image
+                  source={{ uri: images[i] }}
+                  style={{ width: 80, height: 80, borderRadius: 40 }}
+                />
+              </View>
+            ))
+          }
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('ParallaxSwiper', () => ParallaxSwiper);
