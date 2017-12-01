07cc18630e992606a540be346b4380d8bc8b320d
Zach Gibson <zach.gibson@taillight.com>
2017-12-01 11:08:15 -0600

    Make example project name more specific

--- examples/App.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-
-export default class App extends Component<{}> {
-  render() {
-    return <View style={styles.container} />;
-  }
-}
