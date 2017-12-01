45be9bf399f7440d25cc732dcdeb9103e4c48a12
Zach Gibson <zach.gibson@taillight.com>
2017-12-01 10:50:41 -0600

    Add initial examples

---
+++ examples/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component<{}> {
+  render() {
+    return <View style={styles.container} />;
+  }
+}
